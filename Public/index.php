6356a68c67e77de64f3d5e847f94846954919007
mana007777 <e7ifubbb@gmail.com>
2026-02-22 22:02:34 +0300

    Fixing some bugs

--- Public/index.php
+++ Public/index.php
@@ -1,9 +1,7 @@
 <?php
 
-
-namespace app\Public;
 require_once __DIR__.'/../vendor/autoload.php';
-use app\core\Application;
+use app\Core\Application;
 
 
 $app = new Application(dirname(__DIR__));
