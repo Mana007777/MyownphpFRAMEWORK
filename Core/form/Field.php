8c282bd61e99feb3b0d3f6b9753b715b0445c0e6
mana007777 <e7ifubbb@gmail.com>
2026-02-25 22:30:51 +0300

    Fixing everything about validation

--- Core/form/Field.php
+++ Core/form/Field.php
@@ -6,13 +6,22 @@ use app\Core\Model;
 
 class Field{
 
+  public const TYPE_TEXT = "text";
+  public const TYPE_TEXTAREA = "textarea";
+  public const TYPE_RADIO = "radio";
+  public const TYPE_CHECKBOX = "checkbox";
+  public const PASSWORD = "password";
+
+   public string $type;
    public Model $model;
    public string $attribute;
 
 
    public function __construct(Model $model, string $attribute){
+    $this->type = self::TYPE_TEXT;
     $this->model = $model;
     $this->attribute = $attribute;
+
    }
 
    public function __toString()
@@ -25,9 +34,18 @@ class Field{
       %s
     </div>
      ', $this->attribute, 
-     $this->attribute,
+     $this->type,
      $this->model->{$this->attribute},
      $this->model->hasError($this->attribute) ? ' is-invalid' : '',
      $this->model->getFirstErrors($this->attribute));
+
+
+
+   }
+
+
+   public function password(){
+    return $this->type = self::PASSWORD;
+    return $this; 
    }
 }
