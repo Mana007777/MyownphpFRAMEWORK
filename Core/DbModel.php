e3833d4c6c0cae8840586fc84f0cdb8f7a72a345
mana007777 <e7ifubbb@gmail.com>
2026-02-27 02:29:03 +0300

    Implementing findOne function

--- Core/DbModel.php
+++ Core/DbModel.php
@@ -21,6 +21,18 @@ abstract class DbModel extends Model
       return true;
    }
 
+   public function findOne($where){
+      $tableName = static::TableName();
+      $attributes = array_keys($where);
+      $sql = implode("AND",array_map(fn($attr)=>"$attr = :$attr", $attributes));
+      $statment = self::prepare("SELECT * FROM $tableName WHERE $sql");
+      foreach($where as $key => $value){
+         $statment->bindValue(":$key", $value);
+      }
+      $statment->execute();
+      return $statment->fetchObject(static::class);
+      }
+
    public static function prepare($sql){
       return Application::$app->db->pdo->prepare($sql);
    }
