76885776d5ed1121df0c0ebe8da9d95140504e3f
mana007777 <e7ifubbb@gmail.com>
2026-02-27 02:02:53 +0300

    starting login

--- Views/login.php
+++ Views/login.php
@@ -1,15 +1,14 @@
-<form method="post" action="">
-  <div class="mb-3">
-    <label class="form-label">Subject</label>
-    <input type="text" class="form-control">
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Email</label>
-    <input type="email" class="form-control">
-  </div>
-    <div class="mb-3">
-    <label class="form-label">Message</label>
-    <textarea class="form-control" rows="3"></textarea>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<h1>Login to your account</h1>
+<?php
+
+use app\Core\form\Form;
+
+$form = Form::begin('', 'post') ?>
+
+<?php echo $form->field($model, 'firstname') ?>
+<?php echo $form->field($model, 'lastname') ?>
+<?php echo $form->field($model, 'email') ?>
+<?php echo $form->field($model, 'password')->password() ?>
+<?php echo $form->field($model, 'confirm_password')->password() ?>
+<?php Form::end() ?>
+<button type="submit" class="btn btn-primary">Submit</button>
