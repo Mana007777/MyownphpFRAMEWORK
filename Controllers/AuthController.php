70ee3b3a5de40548adde015fa7fe58358c677eb3
mana007777 <e7ifubbb@gmail.com>
2026-02-28 01:32:06 +0300

    Staring profile

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -52,4 +52,8 @@ class AuthController extends Controller
         Application::$app->logout();
         $response->redirect('/'); 
     }
+
+    public function profile(){
+        return $this->render('profile');
+    }
 }
