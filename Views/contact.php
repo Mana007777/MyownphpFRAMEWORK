8ad054b355d62e443a21908b4c3e1dce1bccd8c5
mana007777 <e7ifubbb@gmail.com>
2026-03-02 01:39:55 +0300

    TextArea

--- Views/contact.php
+++ Views/contact.php
@@ -1,6 +1,7 @@
 <?php
 
 use app\Core\form\Form;
+use app\Core\form\TextAreaField;
 
 $this->title = "Contact";
 
@@ -9,6 +10,6 @@ $this->title = "Contact";
 <?php $form = Form::begin('','post') ?>
 <?php echo $form->field($model,'subject') ?>
 <?php echo $form->field($model,'email') ?>
-<?php echo $form->field($model,'body') ?>
+<?php echo new TextAreaField($model , 'body') ?>
 <button type="submit" class="btn btn-primary">Submit</button>
 <?php Form::end() ?>
